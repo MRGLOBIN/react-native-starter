44db189da3c50e5a69b5c0da3c228a86857f3b12
Muhammad Asim <asimqwe2@gmail.com>
2024-09-18 22:22:11 +0500

    add: modal for the input and gaol items

--- App.jsx
+++ App.jsx
@@ -1,14 +1,59 @@
-import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View, FlatList, Button } from "react-native";
+import GoalItem from "./component/goal-item";
+import GoalInput from "./component/goal-input";
 
 export default function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [userGoals, setUserGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+
+  const startAddGoalHandler = () => {
+    setIsModalVisible(true);
+  };
+
+  const addGoalHandler = () => {
+    setUserGoals((previousGoals) => [
+      ...previousGoals,
+      { text: enteredGoalText, key: Math.random().toString() },
+    ]);
+
+    setEnteredGoalText("");
+    setIsModalVisible(false);
+  };
+
+  const onDeleteItem = (key) => {
+    setUserGoals((previousGoals) => {
+      return previousGoals.filter((goal) => {
+        return goal.key !== key;
+      });
+    });
+  };
+
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder="Your course goal!" />
-      </View>
-      <Button title="Add Goal" />
-      <View>
-        <Text>List of goals...</Text>
+      <Button
+        title="Add New Goal"
+        color="#5e0acc"
+        onPress={startAddGoalHandler}
+      />
+      <GoalInput
+        isModalVisible={isModalVisible}
+        enteredGoalText={enteredGoalText}
+        addGoalHandler={addGoalHandler}
+        goalInputHandler={goalInputHandler}
+      />
+      <View style={styles.goalContainer}>
+        <FlatList
+          data={userGoals}
+          renderItem={(goal) => {
+            return <GoalItem goal={goal.item} onDeleteItem={onDeleteItem} />;
+          }}
+        />
       </View>
     </View>
   );
@@ -16,6 +61,11 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  goalContainer: {
+    flex: 5,
   },
 });
